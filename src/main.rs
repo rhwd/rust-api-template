addd5e70389aa91dfc135edba3eba05183e65627
Kaynã de Camargo <kayna.khad@gmail.com>
2024-03-06 20:41:32 -0300

    style: move fn main up

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,6 @@ use dotenv::dotenv;
 
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
-async fn health_checker_handler() -> impl IntoResponse {
-    const MESSAGE: &str = "Simple CRUD API with Rust, SQLX, Postgres,and Axum";
-
-    let json_response = serde_json::json!({
-        "status": "success",
-        "message": MESSAGE
-    });
-
-    Json(json_response)
-}
-
-pub struct AppState {
-    db: Pool<Postgres>,
-}
-
 #[tokio::main]
 async fn main() {
     dotenv().ok();
@@ -48,4 +33,19 @@ async fn main() {
     println!("🚀 Server started successfully");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn health_checker_handler() -> impl IntoResponse {
+    const MESSAGE: &str = "Simple CRUD API with Rust, SQLX, Postgres,and Axum";
+
+    let json_response = serde_json::json!({
+        "status": "success",
+        "message": MESSAGE
+    });
+
+    Json(json_response)
+}
+
+pub struct AppState {
+    db: Pool<Postgres>,
 }
