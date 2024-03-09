083ec00909e54805c29d2e5d2fd0fc9c0ce2efa5
Kaynã de Camargo <kayna.khad@gmail.com>
2024-03-09 00:40:14 -0300

    feat: remove logic from main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,11 @@
 mod database;
-use axum::{response::IntoResponse, routing::get, Json, Router};
+mod routes;
+mod utils;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/api/healthchecker", get(health_checker_handler))
-        .with_state(database::connection().await);
-
+    let router = utils::router::initialize_routes().await;
     println!("Server started successfully.");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
-
-async fn health_checker_handler() -> impl IntoResponse {
-    const MESSAGE: &str = "Simple CRUD API with Rust, SQLX, Postgres,and Axum";
-
-    let json_response = serde_json::json!({
-        "status": "success",
-        "message": MESSAGE
-    });
-
-    Json(json_response)
+    axum::serve(listener, router).await.unwrap();
 }
