d37a6e631ee350518c35e43abb02b15049fa7b0c
Kaynã de Camargo <kayna.khad@gmail.com>
2024-03-09 14:39:44 -0300

    feat: add config folder

--- src/config/router.rs
+++ src/config/router.rs
@@ -1,4 +1,4 @@
-use crate::database;
+use crate::config::database;
 use crate::routes;
 use axum::Router;
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-mod database;
+mod config;
 mod routes;
-mod utils;
 
 #[tokio::main]
 async fn main() {
-    let router = utils::router::initialize_routes().await;
+    let router = config::router::initialize_routes().await;
     println!("Server started successfully.");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, router).await.unwrap();
