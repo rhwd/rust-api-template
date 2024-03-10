e806c5a36ff6a7c4c87fb0bb567dfb72459736d9
Kaynã de Camargo <kayna.khad@gmail.com>
2024-03-10 20:40:30 -0300

    feat: add create method to user

--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -1,11 +1,19 @@
-use crate::models::user::User;
-use axum::{extract::Path, http::StatusCode, response::IntoResponse, Json};
+use crate::{
+    models::user::{CreateUser, User},
+    structs::app_state::AppState,
+};
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
+};
+use serde_json::json;
+use std::sync::Arc;
 
 pub async fn get_one(
     Path(id): Path<uuid::Uuid>,
 ) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
-    println!("I'm being called!!");
-
     let user = User {
         id,
         name: "Joe".to_string(),
@@ -13,4 +21,31 @@ pub async fn get_one(
     };
     let json_response = serde_json::json!(user);
     Ok(Json(json_response))
+}
+
+pub async fn create(
+    State(data): State<Arc<AppState>>,
+    Json(body): Json<CreateUser>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let query_result = sqlx::query_as!(
+        User,
+        "INSERT INTO users (name,email) VALUES ($1, $2) RETURNING *",
+        body.name.to_string(),
+        body.email.to_string()
+    )
+    .fetch_one(&data.db)
+    .await;
+
+    match query_result {
+        Ok(user) => {
+            let user_response = json!(user);
+            return Ok((StatusCode::CREATED, Json(user_response)));
+        }
+        Err(e) => {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({"status": "error","message": format!("{:?}", e)})),
+            ));
+        }
+    }
 }
