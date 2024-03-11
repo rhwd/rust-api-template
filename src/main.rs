7e6e54ffb9e7f094d01fe23f1e9bd8416045ffb1
Kaynã de Camargo <kayna.khad@gmail.com>
2024-03-10 21:00:02 -0300

    fix: improve listener error message

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,14 @@ pub mod utils;
 #[tokio::main]
 async fn main() {
     let router = config::router::initialize_routes().await;
-    println!("Server started successfully.");
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
-    axum::serve(listener, router).await.unwrap();
+    match tokio::net::TcpListener::bind("0.0.0.0:8000").await {
+        Ok(listener) => {
+            println!("Server listening at: {}", listener.local_addr().unwrap());
+            axum::serve(listener, router).await.unwrap();
+        }
+        Err(err) => {
+            println!("Failed to start server. {:?}", err);
+            std::process::exit(1);
+        }
+    };
 }
