26a0d037b6fe3e68e065f1157075ff86bd4e30f8
Kaynã de Camargo <kayna.khad@gmail.com>
2024-03-30 17:09:12 -0300

    feat: add jwt claim

---
+++ src/utils/jwt_claim.rs
@@ -0,0 +1,27 @@
+use axum::{extract::FromRequestParts, http::request::Parts, RequestPartsExt};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    username: String,
+    exp: usize,
+}
+
+#[async_trait]
+impl<S> FromRequestParts<S> for Claims
+where
+    S: Send + Sync,
+{
+    type Rejection = AuthError;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        let TypedHeader(Authorization(bearer)) = parts
+            .extract::<TypedHeader<Authorization<Bearer>>>()
+            .await
+            .map_err(|_| AuthError::InvalidToken)?;
+        let token_data = decode::<Claims>(bearer.token(), &KEYS.decoding, &Validation::default())
+            .map_err(|_| AuthError::InvalidToken)?;
+
+        Ok(token_data.claims)
+    }
+}
