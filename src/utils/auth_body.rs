fa1d542ad5d4b2f1307272f9fd04f7726831d32c
Kaynã de Camargo <kayna.khad@gmail.com>
2024-03-30 17:23:43 -0300

    feat: add auth body

---
+++ src/utils/auth_body.rs
@@ -0,0 +1,14 @@
+#[derive(Debug, Serialize)]
+struct AuthBody {
+    access_token: String,
+    token_type: String,
+}
+
+impl AuthBody {
+    fn new(access_token: String) -> Self {
+        Self {
+            access_token,
+            token_type: "Bearer".to_string(),
+        }
+    }
+}
