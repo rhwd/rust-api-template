1713b786e0d799855a6a624a0661af9ebe1127aa
Kaynã de Camargo <kayna.khad@gmail.com>
2024-04-04 23:05:09 -0300

    feat: include password hash to user migration

--- migrations/20240310164440_users.up.sql
+++ migrations/20240310164440_users.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL
 );
