316b661c40c02025226ff94ba6e9ed20e640ab14
Kaynã de Camargo <kayna.khad@gmail.com>
2024-04-27 02:15:48 -0300

    Fix: writing cookies is not async

--- src/utils/use_signed_cookies.rs
+++ src/utils/use_signed_cookies.rs
@@ -1,7 +1,7 @@
 use axum_extra::extract::cookie::{Cookie, Key, SignedCookieJar};
 
-pub async fn set_signed_cookie(key: String, value: String) -> SignedCookieJar{
-  let safe_key = Key::generate();
-  let jar = SignedCookieJar::new(safe_key);
-  jar.add(Cookie::new(key, value))
+pub fn set_signed_cookie(key: String, value: String) -> SignedCookieJar {
+    let safe_key = Key::generate();
+    let jar = SignedCookieJar::new(safe_key);
+    jar.add(Cookie::new(key, value))
 }
