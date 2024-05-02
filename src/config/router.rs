7665b6baede05671670c1c6ea235690c3778f52f
Kaynã de Camargo <kayna.khad@gmail.com>
2024-05-01 22:54:25 -0300

    Feat: use app state correctly on routes

--- src/config/router.rs
+++ src/config/router.rs
@@ -1,8 +1,10 @@
-use crate::{config::database, routes};
+use crate::routes;
 use axum::Router;
 
+use super::app_state::app_state;
+
 pub async fn initialize_routes() -> Router {
-    let app_state = database::connection().await;
+    let app_state = app_state().await;
     Router::new()
         .merge(routes::healthchecker::create_route())
         .merge(routes::user::create_routes(app_state))
