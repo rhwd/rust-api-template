45b76fdde8f8d94632c21e20249fdf598644dab4
Kaynã de Camargo <kayna.khad@gmail.com>
2024-06-02 23:58:03 -0300

    Feat: Create a auth route

--- src/structs/app_state.rs
+++ src/structs/app_state.rs
@@ -1,6 +1,7 @@
 use axum_extra::extract::SignedCookieJar;
 use sqlx::{Pool, Postgres};
 
+#[derive(Clone)]
 pub struct AppState {
     pub db: Pool<Postgres>,
     pub signed_jar: SignedCookieJar
--- src/utils/session.rs
+++ src/utils/session.rs
@@ -1,16 +1,15 @@
 use uuid::Uuid;
-
-use crate::config::database;
+use crate::{config::database, models::user::User};
 
 pub async fn create(user_id: Uuid) -> Uuid{
     let db_pool = database::connection().await;
 
-    let session = sqlx::query!(
+    let query_result = sqlx::query!(
         "INSERT INTO sessions (user_id) VALUES ($1) RETURNING *",
         user_id
     ).fetch_one(&db_pool).await;
 
-    match session {
+    match query_result {
         Ok(session) => {
             return session.id;
         }
@@ -18,5 +17,26 @@ pub async fn create(user_id: Uuid) -> Uuid{
             panic!("Failed to create session. {:?}", e);
         }
         
+    }
+}
+
+pub async fn authenticate(session_id: Uuid) -> User{
+    let db_pool = database::connection().await;
+
+    let query_result = sqlx::query_as!(
+        User,
+        "SELECT name, email, id FROM users WHERE users.id = (SELECT user_id FROM sessions WHERE id = ($1))",
+        session_id
+    )
+    .fetch_one(&db_pool)
+    .await;
+
+    match query_result {
+        Ok(user) => {
+            return user;
+        }
+        Err(e) => {
+            panic!("Failed to authenticate session. {:?}", e);
+        }
     }
 }
