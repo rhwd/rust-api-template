2cf46878bb4c92db0f476559c5fc160f28304145
Kaynã de Camargo <kayna.khad@gmail.com>
2024-06-06 22:40:58 -0300

    Fix: Remove signed jar method

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,4 +1,3 @@
 pub mod database;
 pub mod router;
-pub mod signed_cookie_jar;
 pub mod app_state;
--- src/config/signed_cookie_jar.rs
+++
@@ -1,7 +0,0 @@
-use axum_extra::extract::cookie::{ Key, SignedCookieJar};
-
-pub fn signed_jar() -> SignedCookieJar {
-  let safe_key = Key::generate();
-  let jar = SignedCookieJar::new(safe_key);
-  jar.clone()
-}
